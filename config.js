9ba417ddbe532fd91bf64e4632dbe7b85563a1b6
NoMedia14 <domenik.nosal@gmx.net>
2026-03-03 09:42:04 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxBCBta8uZgk-5sJx3ExoJxW1tc2I9240",
+  authDomain: "boardflow-9a92f.firebaseapp.com",
+  projectId: "boardflow-9a92f",
+  storageBucket: "boardflow-9a92f.firebasestorage.app",
+  messagingSenderId: "795223942258",
+  appId: "1:795223942258:web:1420eacadf3e58f73a4eeb"
+};
